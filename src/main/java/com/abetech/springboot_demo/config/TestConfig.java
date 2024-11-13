21e112b70e7bd76f56ef681db29be7536834888f
Leo Abe <47789494+leonardoabe@users.noreply.github.com>
2024-11-13 16:34:00 -0300

    Adding OrderStatus enum to order

--- src/main/java/com/abetech/springboot_demo/config/TestConfig.java
+++ src/main/java/com/abetech/springboot_demo/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.abetech.springboot_demo.config;
 
 import com.abetech.springboot_demo.entities.Order;
 import com.abetech.springboot_demo.entities.User;
+import com.abetech.springboot_demo.entities.enums.OrderStatus;
 import com.abetech.springboot_demo.repositories.OrderRepository;
 import com.abetech.springboot_demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2024-11-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-11-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-11-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-11-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o2 = new Order(null, Instant.parse("2024-11-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2024-11-22T15:21:22Z"), OrderStatus.PAID, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
