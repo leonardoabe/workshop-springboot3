2cc1e15339079113d8b092f741a80d80b8311e90
Leo Abe <47789494+leonardoabe@users.noreply.github.com>
2024-11-22 19:20:33 -0300

    Implementing user delete endpoint

--- src/main/java/com/abetech/springboot_demo/resources/UserResource.java
+++ src/main/java/com/abetech/springboot_demo/resources/UserResource.java
@@ -4,6 +4,7 @@ import com.abetech.springboot_demo.entities.User;
 import com.abetech.springboot_demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,4 +48,11 @@ public class UserResource {
 
         return ResponseEntity.created(uri).body(user);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
